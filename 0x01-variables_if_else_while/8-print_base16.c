95e101895211dd8e2b01b5cb392bd3a588c94a6c
Iwamgad <dagmawiharry2@gmail.com>
2022-04-08 03:31:12 -0700

    8. Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,9 +5,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{  
+{
 char ch;
-for (ch = 'z' ; ch >= 'a' ; ch--)
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+}
+for (ch = 'a' ; ch <= 'f' ; ch++)
 {
 putchar(ch);
 }

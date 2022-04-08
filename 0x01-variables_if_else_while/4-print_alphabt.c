71091bb53a769c81becb3fd9672d10d6b5e3d828
Iwamgad <dagmawiharry2@gmail.com>
2022-04-08 02:54:47 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 after printing the function
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+putchar(ch);
+}
+putchar('\n');
+return (0);
+}

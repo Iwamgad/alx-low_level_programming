be0e20bb6d195ad53280b9d3e3aadabe8ff2a986
Iwamgad <dagmawiharry2@gmail.com>
2022-04-12 01:16:21 -0700

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets in lowercase ten times
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int i = 1;
+while (i <= 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+if (i != 10)
+_putchar('\n');
+i++;
+}
+}

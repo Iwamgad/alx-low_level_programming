3422fc76f708b4644416967bbcaec7962744d9f1
Iwamgad <dagmawiharry2@gmail.com>
2022-04-13 03:51:43 -0700

    15. Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,14 +7,17 @@
  */
 int main(void)
 {
-long a[100];
+long a[35];
 int i;
 long sum = 0;
 a[0] = 1;
 a[1] = 2;
-for (i = 2; i < 100; i++)
+for (i = 2; i < 35; i++)
 {
 a[i] = a[i - 2] + a[i - 1];
+}
+for (i = 0; i < 35; i++)
+{
 if (a[i] < 4000000 && a[i] % 2 == 0)
 {
 sum += a[i];

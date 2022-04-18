6af30d3b063a3b5d7522c2f98da10b0da1b7e946
Iwamgad <dagmawiharry2@gmail.com>
2022-04-18 11:45:18 -0700

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,31 +3,29 @@
 #include <time.h>
 
 /**
- * main - program that generates random valid passwords for the program 101-
- * crackme
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int sum = 2772;
-int n;
-srand(time(0));
-while (sum)
+int i, sum, n;
+int pass[100];
+sum = 0;
+srand(time(NULL));
+for (i = 0; i < 100; i++)
 {
-n = rand() % 100;
-if (n <= 94)
-n += 32;
-else
-continue;
-if (sum - n == 0)
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if ((2772 - sum) - '0' < 78)
 {
-sum -= n;
-printf("%c", n);
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
 }
-else if (sum - n - 32 > 0)
-sum -= n;
-printf("%c", n);
 }
 return (0);
 }

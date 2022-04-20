499bdcac76c3e209a0eb23d1512d6f205a117a0e
Iwamgad <dagmawiharry2@gmail.com>
2022-04-20 05:36:34 -0700

    I did starting from task 3 upto 7

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _strcmp - copies a string
+ * @s1: a pointer to a string
+ * @s2: a pointer to a string
+ * Return: returns a -15 if s1 < s2, 15 if s1 > s2 & 0 if s1 = s2
+ */
+int _strcmp(char *s1, char *s2)
+{
+int sizes1, sizes2, i = 0, j = 0;
+while (i >= 0)
+{
+if (s1[i] == '\0')
+{
+sizes1 = i;
+break;
+}
+i++;
+}
+i = 0;
+while (i >= 0)
+{
+if (s2[i] == '\0')
+{
+sizes2 = i;
+break;
+}
+i++;
+}
+for (j = 0; j < sizes1 && j < sizes2; j++)
+{
+if (s1[j] < s2[j])
+{
+return (-15);
+}
+else if (s1[j] > s2[j])
+{
+return (15);
+}
+else
+{
+return (0);
+}
+}
+return (0);
+}

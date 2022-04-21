d1c2f1a0d15848fb013b37a72fd030e0f273b788
Iwamgad <dagmawiharry2@gmail.com>
2022-04-20 23:59:01 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,4 +12,5 @@ char *leet(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *rot13(char *);
 void print_number(int n);
+void print_buffer(char *b, int size);
 #endif /* MAIN_H */

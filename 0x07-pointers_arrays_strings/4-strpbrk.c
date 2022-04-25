5be63f6d2159179634ef857a7b753b73716e5604
Iwamgad <dagmawiharry2@gmail.com>
2022-04-25 08:24:24 -0700

    I did task 4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,7 +12,7 @@ char *_strpbrk(char *s, char *accept)
 {
 	unsigned int flag, index, i, j;
 
-	for (i = 0; s[i] != '0'; i++)
+	for (i = 0; s[i] >= '0'; i++)
 	{
 		flag = 0;
 		for (j = 0; accept[j] != '\0'; j++)

f61e73868cd9cabfd3031c69b90da966cc046658
Iwamgad <dagmawiharry2@gmail.com>
2022-04-25 06:55:44 -0700

    I did task 3 again

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,14 +10,18 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j = 0;
+	unsigned int i, j = 0, retLength = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; accept[i] != '\0'; i++)
 	{
-		if (accept[j] == s[i])
+		for (j = 0; s[j] != '\0'; j++)
 		{
-			return (i + 1);
+			if (accept[i] == s[j])
+			{
+				retLength++;
+				break;
+			}
 		}
 	}
-	return (i);
+	return (retLength + 1);
 }

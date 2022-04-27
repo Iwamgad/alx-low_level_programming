58b13d90d79ae07dfceceee1891fe0d1518e5fa8
Iwamgad <dagmawiharry2@gmail.com>
2022-04-27 09:54:52 -0700

    I did task 2 again

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,14 +6,13 @@
  * Return: returns the length of the string s
  */
 
-int length;
-
 int _strlen_recursion(char *s)
 {
+	int length = 0;
+
 	if (*s != '\0')
 	{
-		length++;
-		_strlen_recursion(s + 1);
+		length += _strlen_recursion(s + 1) + 1;
 	}
 	return (length);
 }

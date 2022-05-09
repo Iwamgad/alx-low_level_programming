b2d5113582d66f80d0681620899e485f30908d0e
Iwamgad <dagmawiharry2@gmail.com>
2022-05-09 12:14:51 -0700

    I did task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,16 @@
 #ifndef DOG
 #define DOG
+
+/**
+ * struct dog - structure for a dog object
+ * @name: name member
+ * @age: age memeber
+ * @owner: owner member
+ * Description: structure called "dog" that stores the name, age and
+ *              the name of its owner
+ */
+
+
 struct dog
 {
 	char *name;

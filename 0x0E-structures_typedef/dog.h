41691feadde863299cca2672e4e3c3531d779b03
Iwamgad <dagmawiharry2@gmail.com>
2022-05-09 13:22:10 -0700

    I did task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -4,7 +4,6 @@
 /**
  * dog - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 /**
@@ -15,15 +14,12 @@ typedef struct dog dog_t;
  * Description: structure called "dog" that stores the name, age and
  *              the name of its owner
  */
-
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);

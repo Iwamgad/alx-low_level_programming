ec100f6cd6e76ef11d34f47b64303003560441c0
Iwamgad <dagmawiharry2@gmail.com>
2022-05-09 12:55:25 -0700

    I did task 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,12 @@
 #define DOG
 
 /**
+ * dog - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+/**
  * struct dog - structure for a dog object
  * @name: name member
  * @age: age memeber
@@ -17,4 +23,9 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif /*#ifndef DOG*/

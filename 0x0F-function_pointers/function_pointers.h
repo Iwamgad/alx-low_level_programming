6dd32ee532a30be29cfa0a33e943a14d98b9ecdb
Iwamgad <dagmawiharry2@gmail.com>
2022-05-11 13:01:22 -0700

    I did task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,7 @@
 #ifndef FUNCPOINTERS
 #define FUNCPOINTERS
-
-
-
+void _putchar(char *c);
 void print_name(char *name, void (*f)(char *));
-
-
-
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif /*#ifndef FUNCPOINTERS*/

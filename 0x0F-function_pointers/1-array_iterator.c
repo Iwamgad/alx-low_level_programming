3b1435f59497d85c7b0678f78330a0d0d9db3147
Iwamgad <dagmawiharry2@gmail.com>
2022-05-11 14:14:00 -0700

    I did task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+/**
+ * array_iterator - executes a function given
+ *                  as a parameter on each element of an array
+ * @array: a pointer to an integer
+ * @size: takes in the size of the array
+ * @action: a pointer to a function
+ * Return: returns nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+	if (array && size > 0 && action)
+	{
+		for (i = 0; i < size; i++)
+		{
+			(*action)(*(array + i));
+		}
+	}
+}

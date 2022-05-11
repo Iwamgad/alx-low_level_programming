7d3165f5f84e269962359934e57dc6b1ae136ca4
Iwamgad <dagmawiharry2@gmail.com>
2022-05-11 14:58:08 -0700

    I did task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+/**
+ * int_index - searches for an integer
+ * @array: a pointer to the first integer of an array
+ * @size: takes in the size of the array
+ * @cmp: a pointer to a function
+ * Return: returns the index of the first element
+ *         for which the cmp function does not return 0
+ *         If no element matches, return -1
+ *         If size <= 0, return -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, index;
+
+	if (size <= 0)
+		return (-1);
+	if (array && size > 0 && cmp)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (((*cmp)(*(array + i))) == 1)
+			{
+				index = i;
+				break;
+			}
+		}
+	}
+	return (index);
+}

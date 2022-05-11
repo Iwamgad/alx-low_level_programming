a78e8551b72e4a4412bd24e5198d0edd82df448d
Iwamgad <dagmawiharry2@gmail.com>
2022-05-11 13:08:01 -0700

    I did task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,12 +1,12 @@
-void (*f)(char *);
-
 /**
  * print_name - prints a name
  * @name: a pointer to a string
  * @f: a pointer to a function
+ * Return: returns nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	(*f)(name);
+	if(name && f)
+		(*f)(name);
 }

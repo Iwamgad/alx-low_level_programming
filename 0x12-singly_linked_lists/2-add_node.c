462d36fd47d179755fd87d50d2b2c01a04d63e92
Iwamgad <dagmawiharry2@gmail.com>
2022-05-19 04:18:27 -0700

    I did task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+ * add_node - returns the number of elements in a linked list_t list
+ * @head: takes in a pointer to the head of the list_t list
+ * @str: takes in a string to be added to the list_t list
+ * Return: the address of the new element, or NULL if it failed
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *duplicate;
+	int len;
+	list_t *addresNew;
+
+	addresNew = malloc(sizeof(list_t));
+	if (addresNew == NULL)
+		return (NULL);
+	duplicate = strdup(str);
+	if (duplicate == NULL)
+	{
+		free(addresNew);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	addresNew->str = duplicate;
+	addresNew->len = len;
+	addresNew->next = *head;
+
+	*head = addresNew;
+	return (addresNew);
+}

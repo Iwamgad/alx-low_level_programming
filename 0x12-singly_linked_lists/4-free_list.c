55f863266a64a87ab1343e523715bf955202e38e
Iwamgad <dagmawiharry2@gmail.com>
2022-05-19 04:41:05 -0700

    I did task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - frees a list_t list
+ * @head: takes in a pointer to the head node of the list_t list
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+	
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}

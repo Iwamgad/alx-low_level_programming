08d820b55733b80e25c4033cb13f3a1d85d81c8d
Iwamgad <dagmawiharry2@gmail.com>
2022-05-23 13:36:07 -0700

    I did task 1

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elements in a linked list_t list
+ * @h: a pointer to the list
+ * Return: returns the number of elements
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t nodeCount = 0;
+
+	while (h != 0)
+	{
+		h = h->next;
+        nodeCount++;
+	}
+	return (nodeCount);
+}

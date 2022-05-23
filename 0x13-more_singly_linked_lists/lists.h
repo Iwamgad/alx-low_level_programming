5354e659715dd9c3ef95296dd3ec7a587dfa0588
Iwamgad <dagmawiharry2@gmail.com>
2022-05-23 13:22:11 -0700

    I did task 0

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,7 +1,7 @@
 #ifndef MORE_SINGLY_LINKED_LIST
 #define MORE_SINGLY_LINKED_LIST
-#include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer

2896e6fad05a3d6abdce5ecca419151b38920d6c
Iwamgad <dagmawiharry2@gmail.com>
2022-05-24 01:03:05 -0700

    I did task 0 again

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,11 +10,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodeCount = 0;
 
-	while (h != 0)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
 		nodeCount++;
+		h = h->next;
 	}
 	return (nodeCount);
 }

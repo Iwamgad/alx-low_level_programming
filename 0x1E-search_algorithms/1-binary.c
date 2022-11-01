42b2a7d27a267e81c4cc353c0d422fedf983d7d1
Iwamgad <dagmawiharry2@gmail.com>
2022-11-01 07:34:36 -0700

    I edited task 1 again

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -13,7 +13,7 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	int l = 0, r = (int) (size - 1), mid, i = l, j = r;
+	size_t l = 0, r = (int) (size - 1), mid, i = l, j = r;
 
 	if (array == NULL)
 		return (-1);

e346772b43ad022c9089b2765e4140193c3601a4
Iwamgad <dagmawiharry2@gmail.com>
2022-11-02 03:00:46 -0700

    I did task 9

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,42 @@
+#include "search_algos.h"
+
+/**
+ * interpolation_search - searches for a value in an array of integers using
+ *                 the Interpolation search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: return the first index where value is located
+ *         It returns -1 If value is not present in array or
+ *         if array is NULL
+ */
+
+int interpolation_search(int *array, size_t size, int value)
+{
+	int l, r, pos;
+
+	l = 0;
+	r = (int) (size - 1);
+
+	if (array == NULL)
+		return (-1);
+	while (l <= r)
+	{
+		pos = l + ((value - array[l]) / (array[r] - array[l])) * (r - l);
+		if (pos >= (int) size)
+		{
+			printf("Value checked array[%d] is out of range \n", pos);
+			return (-1);
+		}
+		if (array[pos] == value)
+		{
+			return (pos);
+		}
+		else if (array[pos] > value)
+			r = pos - 1;
+		else
+			l = pos + 1;
+		printf("Value checked array[%d] = [%d]\n", l, array[l]);
+	}
+	return (-1);
+}
